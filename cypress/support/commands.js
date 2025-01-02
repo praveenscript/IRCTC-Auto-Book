04ec549fde3457183b54ff685da3286813f5e264
praveen <praveenkumarjkr@gmail.com>
2025-01-02 11:37:45 +0530

    Extend Tatkal booking wait time from 5 to 10 minutes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -187,7 +187,7 @@ function BOOK_UNTIL_TATKAL_OPENS(
         // wait for exact time
         // cy.task("log", "Waiting for the exact time of opening of TATKAL...");
         const exactTimeToOpen = tatkalOpenTimeForToday(TRAIN_COACH);
-        cy.get("div.h_head1", { timeout: 300000 }).should(
+        cy.get("div.h_head1", { timeout: 600000 }).should(
             "include.text",
             exactTimeToOpen
         );
